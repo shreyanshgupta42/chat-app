9594e6387d2ed4b388384ad44b4d590777b48859
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-06-01 02:11:44 +0530

    linked facebook and google

--- src/components/dashboard/Index.js
+++ src/components/dashboard/Index.js
@@ -1,14 +1,23 @@
 /* eslint-disable arrow-body-style */
 import React from 'react';
-import { Button, Divider, Drawer } from 'rsuite';
+import { Alert, Button, Divider, Drawer } from 'rsuite';
 import { useProfile } from '../../context/profile.context';
+import { database } from '../../misc/firebase';
 import EditableInput from '../EditableInput';
+import ProviderBlock from './ProviderBlock';
 
 const Dashboard = ({ onSignOut }) => {
   const { profile } = useProfile();
 
   const onSave = async newData => {
-    console.log(newData);
+    //   updating the database with new name
+    const userNicknameRef=database.ref(`profiles/${profile.uid}`).child('name');
+    try {
+        await userNicknameRef.set(newData);
+        Alert.info('nickname has been updated',4000);
+    } catch (error) {
+        Alert.error(error.message,4000)
+    }
   };
   return (
     <>
@@ -17,6 +26,7 @@ const Dashboard = ({ onSignOut }) => {
       </Drawer.Header>
       <Drawer.Body>
         Hey, {profile.name}
+        <ProviderBlock/>
         <Divider />
         <EditableInput
           name='nickname'
