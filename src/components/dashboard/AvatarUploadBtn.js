13668b026ba754db1febcf1b8c86451d82074446
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-06-01 22:30:07 +0530

    uploaded user avatar

--- src/components/dashboard/AvatarUploadBtn.js
+++ src/components/dashboard/AvatarUploadBtn.js
@@ -1,27 +1,69 @@
 /* eslint-disable arrow-body-style */
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Alert, Button, Modal } from 'rsuite';
 import AvatarEditor from 'react-avatar-editor';
 import { useModelState } from '../../misc/custom-hooks';
+import { database, storage } from '../../misc/firebase';
+import { useProfile } from '../../context/profile.context';
 
 const fileInputTypes = '.png, .jpeg, .jpg';
 const acceptedfiletypes = ['image/png', 'image/jpeg', 'image/pjpeg'];
 const isValidFile = file => acceptedfiletypes.includes(file.type);
+const getBlob = canvas => {
+  return new Promise((resolve, reject) => {
+    canvas.toBlob(blob => {
+      if (blob) {
+        resolve(blob);
+      } else {
+        reject(new Error('file process error'));
+      }
+    });
+  });
+};
 const AvatarUploadBtn = () => {
-  const {isOpen,open,  close} = useModelState();
+  const { isOpen, open, close } = useModelState();
+  const { profile } = useProfile();
   const [img, setImg] = useState(null);
+  const avatarEditorRef = useRef();
+  const [isloading, setIsLoading] = useState(false);
   const onfileInputChange = ev => {
     const currfiles = ev.target.files;
     if (currfiles.length === 1) {
       const file = currfiles[0];
       if (isValidFile(file)) {
         setImg(file);
-        open()
+        open();
       } else {
         Alert.warning('you selected the wrong file', 4000);
       }
     }
   };
+  const onUploadClick = async () => {
+    // we use current below because we want the current element form avatarEditorRef and it can also be undefined, and the function getImageScaledToCanvas comes from the documentation of avatar editor
+    const canvas = avatarEditorRef.current.getImageScaledToCanvas();
+    setIsLoading(true);
+    try {
+      const blob = await getBlob(canvas);
+      // below is file to store image in firebase
+      const avatarFileRef = storage
+        .ref(`/profile/${profile.uid}`)
+        .child('avatar');
+      const uploadAvatarResult = await avatarFileRef.put(blob, {
+        cacheControl: `public,max-age=${3600 * 24 * 3}`,
+      });
+      const downloadUrl = await uploadAvatarResult.ref.getDownloadURL();
+      const userAvatarRef = database
+        .ref(`/profiles/${profile.uid}`)
+        .child('avatar');
+      await userAvatarRef.set(downloadUrl);
+      setIsLoading(false);
+      Alert.info('avatar has been uploaded', 4000);
+    } catch (error) {
+      setIsLoading(false);
+      Alert.error(error.message, 4000);
+    }
+  };
+
   return (
     <div className="mt-3 text-center">
       <div>
@@ -44,21 +86,27 @@ const AvatarUploadBtn = () => {
           <Modal.Title>Adjust and upload new avatar</Modal.Title>
           <Modal.Body>
             {img && (
-                <div className='d-flex justify-content-center align-items-center h-100'>
-
-              <AvatarEditor
-                image={img}
-                width={200}
-                height={200}
-                border={10}
-                borderRadius={100}
-                rotate={0}
+              <div className="d-flex justify-content-center align-items-center h-100">
+                {/* below edits the image to requirement */}
+                <AvatarEditor
+                  ref={avatarEditorRef}
+                  image={img}
+                  width={200}
+                  height={200}
+                  border={10}
+                  borderRadius={100}
+                  rotate={0}
                 />
-                </div>
+              </div>
             )}
           </Modal.Body>
           <Modal.Footer>
-            <Button block appearance="ghost">
+            <Button
+              block
+              appearance="ghost"
+              onClick={onUploadClick}
+              disabled={isloading}
+            >
               upload new avator
             </Button>
           </Modal.Footer>
--- src/misc/firebase.js
+++ src/misc/firebase.js
@@ -2,6 +2,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth'
 import 'firebase/database'
+import 'firebase/storage'
 
 const config = {
   apiKey: 'AIzaSyCkmj7IWOlBfk32ZYuwApF9f7TjNXld1Hk',
@@ -15,3 +16,4 @@ const config = {
 const app = firebase.initializeApp(config);
 export const auth=app.auth();
 export const database=app.database();
+export const storage=app.storage();
