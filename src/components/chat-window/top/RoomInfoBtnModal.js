6d876da2e1cc83e1143a71545108821a90ef64dc
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-06-04 00:17:24 +0530

    created room description modal

---
+++ src/components/chat-window/top/RoomInfoBtnModal.js
@@ -0,0 +1,35 @@
+/* eslint-disable arrow-body-style */
+import React from 'react';
+import { Button, Modal } from 'rsuite';
+import { useCurrentRoom } from '../../../context/current-room.context';
+import { useModelState } from '../../../misc/custom-hooks';
+
+const RoomInfoBtnModal = () => {
+  const { isOpen, close, open } = useModelState();
+  const name = useCurrentRoom(v => v.name);
+  const description = useCurrentRoom(v => v.description);
+
+  return (
+    <>
+      <Button appearance="link" className="px-0" onClick={open}>
+        Room Information
+      </Button>
+      <Modal show={isOpen} onHide={close}>
+        <Modal.Header>
+          <Modal.Title>About {name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h6 className="mb-1">Description</h6>
+          <p>{description}</p>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button block onClick={close}>
+            close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+};
+
+export default RoomInfoBtnModal;
--- src/components/chat-window/top/index.js
+++ src/components/chat-window/top/index.js
@@ -1,15 +1,41 @@
 /* eslint-disable arrow-body-style */
-import React,{memo} from 'react'
+import React, { memo } from 'react';
+import { Link } from 'react-router-dom';
+import { ButtonToolbar, Icon } from 'rsuite';
 import { useCurrentRoom } from '../../../context/current-room.context';
+import { useMediaQuery } from '../../../misc/custom-hooks';
+import RoomInfoBtnModal from './RoomInfoBtnModal';
 
 // when description is change in data then below will not be rerendered because of memo(memo compares the value change if values changes than only rerenders) , thus only rerenders when name changes
 const Top = () => {
-    const name=useCurrentRoom(v=>v.name);
-    return (
-        <div>
-            {name}
-        </div>
-    )
-}
+  const name = useCurrentRoom(v => v.name);
+  const isMobile = useMediaQuery('(max-width:992px)');
+  return (
+    <div>
+      <div className="d-flex justify-content-between text-align-center">
+        <h4>
+          {/* by componentClass={Link} we wil make Icon clickabe and will lead to a link provided in to property */}
+          <Icon
+            componentClass={Link}
+            to="/"
+            icon="arrow-circle-left"
+            size="2x"
+            className={
+              isMobile
+                ? 'd-inline-block p-0 mr-2 text-blue link-unstyled'
+                : 'd-none'
+            }
+          />
+          <span className="text-disappear">{name}</span>
+        </h4>
+        <ButtonToolbar className='ws-nowrap'>todo</ButtonToolbar>
+      </div>
+      <div className='d-flex justify-content-between align-items-center'>
+          <span>todo</span>
+          <RoomInfoBtnModal/>
+      </div>
+    </div>
+  );
+};
 
-export default memo(Top)
+export default memo(Top);
