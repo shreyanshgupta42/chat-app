e24f04fd0e76d27d855c346c645ed980784a3e80
shreyanshgupta42 <57253725+shreyanshgupta42@users.noreply.github.com>
2021-06-05 00:14:22 +0530

    displayed messages inside chat place

--- src/components/chat-window/messages/index.js
+++ src/components/chat-window/messages/index.js
@@ -1,12 +1,44 @@
 /* eslint-disable arrow-body-style */
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import { database } from '../../../misc/firebase';
+import { transformToArrWithId } from '../../../misc/helpers';
+import MessageItem from './MessageItem';
 
 const Messages = () => {
-    return (
-        <div>
-            messages
-        </div>
-    )
-}
+  const [messages, setMessages] = useState(null);
+  const { chatId } = useParams();
 
-export default Messages
+  const isChatEmpty = messages && messages.length === 0;
+  const canShowMessages = messages && messages.length > 0;
+
+  useEffect(() => {
+    // reference to the messages section in database
+    const messagesRef = database.ref('/messages');
+
+    //  below is a realtime subscription
+    messagesRef
+      .orderByChild('roomId')
+      .equalTo(chatId)
+      .on('value', snap => {
+        const data = transformToArrWithId(snap.val());
+        setMessages(data);
+      });
+
+    // dismounting time
+    return () => {
+      // unsubscribe to the subcribed item which is neccessary
+      messagesRef.off('value');
+    };
+  }, [chatId]);
+
+  return (
+    <ul className="msg-list custom-scroll">
+      {isChatEmpty && <li>no messages yet</li>}
+      {canShowMessages &&
+        messages.map(msg => <MessageItem key={msg.id} message={msg} />)}
+    </ul>
+  );
+};
+
+export default Messages;
